7e2aa724259bcf0999c73415e1c67000537bdac7
Marc G <marc@okidoo.ca>
2022-09-28 16:53:44 -0400

    feature: most up to date scripts + readme

---
+++ README.md
@@ -0,0 +1,26 @@
+# Information
+
+The system works with the help of 2 scripts.
+The first one is a Razor script which collects the vendor items and saves the information in the UO Log files. It runs after every World Save.
+The second script is a NodeJS script which copies the Uo Log file and sends it to https://outlands.shop's server. This script runs every hour but can be configured.
+
+The Uo Log file gets parsed by the server to only keep the last 2 hours. Only the most up to date information about a specific vendor is getting save into the website.
+
+### NodeJS
+
+install the lts version: https://nodejs.org/en/download/
+
+### NodeJS Script
+
+Download and Unzip:
+https://github.com/Nesci28/OutlandsShop-scripts/archive/refs/heads/master.zip
+
+Open a CommandLine:
+cd <UNZIP_FOLDER> && npm i --omit=dev
+
+### Run the NodeJS Script
+
+Open a CommandLine as Administrator (this is necessary because the UO Log file is locked by ClassicUO while the application is running).
+cd <UNZIP_FOLDER>
+node app.js -h 1 -d <CLASSIC_UO_JOURNAL_LOGS_FOLDER>
+ex: node app.js -h 1 -d "C:\Program Files (x86)\Ultima Online Outlands\ClassicUO\Data\Client\JournalLogs"
--- app.js
+++ app.js
@@ -1,39 +1,47 @@
 /* eslint-disable no-console */
 const axios = require("axios");
+const { execSync } = require("child_process");
 const { program } = require("commander");
 const cronTime = require("cron-time-generator");
 const FormData = require("form-data");
 const fs = require("fs");
 const cron = require("node-cron");
 const Os = require("os");
-const sudo = require("sudo-prompt");
 const wdl = require("windows-drive-letters");
 
+const DateUtil = require("./date-util.js");
 const Upload = require("./upload.js");
 
 const URL = "https://api.outlands.shop/vendor?timezone=";
 
-async function main(directory) {
+async function main(directory, hours) {
   const { timeZone } = Intl.DateTimeFormat().resolvedOptions();
-  const copy = await shadowCopy(directory);
-  console.log("copy :>> ", copy);
+  await shadowCopy(directory);
 
-  // const url = `${URL}${timeZone}`;
-  // const upload = new Upload(directory);
+  const url = `${URL}${timeZone}`;
+  const upload = new Upload("./logs");
 
-  // const fileName = await upload.getLatestFilename();
-  // const file = fs.readFileSync(`${directory}/${fileName}`);
-  // const formData = new FormData();
-  // formData.append("file", file, "file.txt");
+  const fileName = await upload.getLatestFilename();
+  const file = fs.readFileSync(`./logs/${fileName}`);
+  const formData = new FormData();
+  formData.append("file", file, "file.txt");
 
-  // try {
-  //   const res = await axios.post(url, formData);
-  //   console.log(
-  //     `success: ${res.data.isSuccess} - items: ${res.data.value.length}`,
-  //   );
-  // } catch (err) {
-  //   console.log("err :>> ", err.response.data.error.message);
-  // }
+  try {
+    const res = await axios.post(url, formData);
+    if (res.data && res.data.value) {
+      console.log(
+        `[${DateUtil.getTimestamp()}] - success: ${
+          res.data.isSuccess
+        } - items: ${res.data.value.length}`,
+      );
+      console.log(`[${DateUtil.getTimestamp()}] - waiting for ${hours} hours`);
+    } else {
+      console.log(`DEBUG [${DateUtil.getTimestamp()}] - ${res}`);
+    }
+  } catch (err) {
+    console.log(err);
+    console.log("err :>> ", err.response.data.error.message);
+  }
 }
 
 program
@@ -49,39 +57,28 @@ const { directory, hours } = program.opts();
 const cronExpression = cronTime.every(+hours).hours();
 
 (async () => {
-  await main(directory);
+  console.log(`[${DateUtil.getTimestamp()}] - script is running`);
+  await main(directory, hours);
 })();
 
 cron.schedule(cronExpression, async () => {
-  await main(directory);
+  await main(directory, hours);
 });
 
 async function shadowCopy(dir) {
-  // const freeLetters = wdl.freeSync();
-  // const letter = freeLetters[0];
-  const letter = "A";
+  const freeLetters = wdl.freeSync();
+  const letter = freeLetters[0];
 
   const subDir = dir.substring(3);
-  console.log("subDir :>> ", subDir);
 
   const arch = Os.arch() === "x64" ? "64" : "32";
-  console.log("arch :>> ", arch);
 
-  const data = `xcopy /s "${letter}:${subDir}" "./logs"`;
+  const data = `xcopy /i "${letter}:\\${subDir}" "./logs"`;
   fs.writeFileSync("./ShadowTask/copy_logs.bat", data);
 
-  return new Promise((resolve, reject) => {
-    sudo.exec(
-      "ls ~",
-      // `./ShadowTask/ShadowTask${arch}.exe ${dir} ${letter} ./ShadowTask/copy_logs.bat`,
-      {},
-      (error) => {
-        if (error) {
-          reject(error);
-        }
+  fs.rmSync("./logs", { recursive: true, force: true });
 
-        resolve();
-      },
-    );
-  });
+  execSync(
+    `.\\ShadowTask\\ShadowTask${arch}.exe "${dir}" ${letter} .\\ShadowTask\\copy_logs.bat`,
+  );
 }
